54b683678c695ec3b70f1ee58069c23a110ed251
BradleyK <bradleyjkennett@gmail.com>
2022-07-06 09:52:33 -0400

    .env add

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection(
+
+   
     {
         host: 'localhost',
-        user: 'root',
-        password: 'Jaxonsdad#1',
-        database: 'employee_tracker'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+        
     },
     console.log('connected to the employee_tracker database')
 );
